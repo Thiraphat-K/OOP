fef19606702ba93ae44de88da2c9cebd31dc5a2d
TK_DN <69136273+Thiraphat-K@users.noreply.github.com>
2021-02-05 16:00:16 +0700

    GregorianCalendar Test

--- OOP/src/LAP/Test_GregorianCalendar.java
+++ OOP/src/LAP/Test_GregorianCalendar.java
@@ -11,5 +11,19 @@ package LAP;
  */
 import java.util.GregorianCalendar;
 public class Test_GregorianCalendar {
-    
+        static void getCurrentDate(GregorianCalendar CD){
+            System.out.println("Year is "+CD.get(GregorianCalendar.YEAR));
+            System.out.println("Month is "+CD.get(GregorianCalendar.MONTH));
+            System.out.println("Date is "+CD.get(GregorianCalendar.DATE));
+            System.out.println("Day of week is "+CD.get(GregorianCalendar.DAY_OF_WEEK));
+        }
+     public static void main(String[] args) {
+        GregorianCalendar CD = new GregorianCalendar();
+        System.out.println("Current year, month, date, and day of week");
+         getCurrentDate(CD);
+         CD.setTimeInMillis(CD.getTimeInMillis()+86400000L);
+         System.out.println("------------\n"+"After specified the elapsed time of one day after current day");
+         getCurrentDate(CD);
+         System.out.println(CD.getTime());
+    }
 }
